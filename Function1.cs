c5de4a94042b0b59fdb1d87f67bdd3970229084f
Levi Cezane <99886084+leviczne@users.noreply.github.com>
2023-02-18 11:49:05 -0300

    Creating Funciton1.cs

---
+++ Function1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace LearnFunction
+{
+    public class Function1
+    {
+        [FunctionName("Function1")]
+        public async Task RunAsync([TimerTrigger("* * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            var client = new HttpClient();
+            var apiEndpoint = "https://localhost:7226/api/Security/register";
+            
+            // Set the data to send in the request body
+            var requestData = new { email = "leviczne.fps@gmail.com", viajanteName = "Levi Testando Function LesGO", password = "leviczne", confirmPassword = "leviczne"};
+            var requestDataJson = JsonSerializer.Serialize(requestData);
+            var requestDataContent = new StringContent(requestDataJson, System.Text.Encoding.UTF8, "application/json");
+
+            // Make the request
+            var response = await client.PostAsync(apiEndpoint, requestDataContent);
+
+            // Process the response
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseContent);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+            }
+        }
+    }
+}
